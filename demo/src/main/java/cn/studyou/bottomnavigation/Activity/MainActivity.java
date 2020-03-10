16887d26b62fbb87b51b2bbe14b75097188291ce
xiaopeng-chen0516 <cjp2566@outlook.com>
2020-03-10 12:20:04 +0800

    第一次整合

--- demo/src/main/java/cn/studyou/bottomnavigation/Activity/MainActivity.java
+++ demo/src/main/java/cn/studyou/bottomnavigation/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.studyou.bottomnavigation.Activity;
 
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
